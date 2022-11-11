fda7152cbfedb42dc114562cad29c14ea53aa8ed
Jon <Jon@Minettes-MacBook-Pro.local>
2022-11-10 22:32:25 -0500

    questions work

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
+//importing external modules
 const fs = require('fs')
 const inquirer = require('inquirer')
 const emailVal = require('email-validator')
 
-
-
+//importing internal modules
 const Engineer = require('./lib/Engineer')
 const Manager= require('./lib/Manager')
 const Intern = require('./lib/Intern')
+//const Template = require('./src/templates')
 
+//empty array to be filled by employee's created
 const roster = []
-// const addEmployee = [{
-//         name: 'addEmployee',
-//         type: 'list',
-//         message: 'What type of employee would you like to add?',
-//         choices: ['Engineer','Intern','My Roster Is Complete'],
-//     }]
 
-// const genQuestions = [{
-//         name: 'name',
-//         type: 'input',
-//         message: "What is your employee's name?",
-//     },
-//     {   name: 'id',
-//         type: 'input',
-//         message: "What is your employee's ID number?",
-//     },
-//     {   name: 'email',
-//         type: 'input',
-//         message: "What is your employee's Email address?",
-//     }]
-//    const manQuestions = [{
-//         name: 'officeNumber',
-//         type: 'input',
-//         message: "What is your Manager's office number?",
-//     }]
-//     const engQuestions= [{
-//         name: 'gitHub',
-//         type: 'input',
-//         message: "What is your Engineer's Git Hub username?",
-//     }]
-//     const inQuestions = [{
-//         name: 'school',
-//         type: 'input',
-//         message: "Where is your Intern currently enrolled?",
-//     }]
+
 function addManager() {
     inquirer.prompt([{
         name: 'name',
@@ -53,7 +22,7 @@ function addManager() {
             if (nameInput) {
                 return true;
             } else {
-                console.log ("Please enter your name!");
+                console.log ("--Please enter your name!--");
                 return false; 
             }
         }
@@ -63,7 +32,7 @@ function addManager() {
         message: "What is your ID number?",
         validate: idInput => {
             if (isNaN(idInput)) {
-                console.log ("Please enter the employee's ID!")
+                console.log ("--Please enter the employee's ID!--")
                 return false; 
             } else {
                 return true;
@@ -77,7 +46,7 @@ function addManager() {
                 if (emailVal.validate(email) === true) {
                 return true;
             } else {
-                console.log ("Please enter a valid email!")
+                console.log ("--Please enter a valid email!--")
                 return false;
             }
     }
@@ -88,7 +57,7 @@ function addManager() {
         message: "What is your Manager's office number?",
         validate: officeInput => {
                 if (isNaN(officeInput)) {
-                    console.log ("Please enter office number!")
+                    console.log ("--Please enter a valid office number!--")
                     return false; 
                 } else {
                     return true;
@@ -100,44 +69,110 @@ function addManager() {
         const manager = new Manager(name, id, email, officeNumber);
 
         roster.push(manager);
-        console.log(manager)
+        console.log(manager);
+        addTeamMember();
     })
-
 }
 
-addManager()
+function addTeamMember() {
+    inquirer.prompt([{
+        name: 'role',
+        type: 'list',
+        message: 'What type of employee would you like to add?',
+        choices: ['Engineer','Intern'],
+    },
+    {
+        name: 'name',
+        type: 'input',
+        message: "What is your name?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log ("--Please enter your name!--");
+                return false; 
+            }
+        }
+    },
+    {   name: 'id',
+        type: 'input',
+        message: "What is your ID number?",
+        validate: idInput => {
+            if (isNaN(idInput)) {
+                console.log ("--Please enter the employee's ID!--")
+                return false; 
+            } else {
+                return true;
+            }
+        }
+    },
+    {   name: 'email',
+        type: 'input',
+        message: "What is your Email address?",
+        validate: email => {
+                if (emailVal.validate(email) === true) {
+                return true;
+            } else {
+                console.log ("--Please enter a valid email!--")
+                return false;
+            }
+        }
+    },
+    {   name: 'github',
+        type: 'input',
+        message: "What is thier GitHub username?",
+        when: (input) => input.role === "Engineer",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log ("--Please enter your GitHub username!--");
+                return false; 
+            }
+        }
+    },
+    {   name: 'school',
+        type: 'input',
+        message: "What school do they attend?",
+        when: (input) => input.role === "Intern",
+        validate: schoolInput => {
+            if (schoolInput) {
+                return true;
+            } else {
+                console.log ("--Please enter your GitHub username!--");
+                return false; 
+            }
+        }
+    },
+    {   name: 'addEmployee',
+        type: 'confirm',
+        message: 'Would you like to add another employee?',
+        default: false
+    }
+              
+    ]).then(teamMemberInfo => {
+        let  {role, name, id, email, github, school, addEmployee} = teamMemberInfo;
+        let employee
 
-// function addTeamMember() {
-//     inquirer.prompt([{
-//         name: 'addEmployee',
-//         type: 'list',
-//         message: 'What type of employee would you like to add?',
-//         choices: ['Engineer','Intern','My Roster Is Complete'],
-//     }]).then(function(results){
-//             switch(results.addEmployee) {
-//                 case 'Manager': manquestion(); break;
-//                 case 'Engineer': inquirer.prompt(genQuestions + engQuestions); break;
-//                 case 'Intern': inquirer.prompt(genQuestions + inQuestions); break;
-//             }
-//     })
-// }
-// function genquestion() {
-//     inquirer.prompt (genQuestions)
-//     const questions = { };
-    
-// }
-// addTeamMember();
-// // function addTeamMember() {
-// //     inquirer.prompt([{
-// //         name: 'addEmployee',
-// //         type: 'list',
-// //         message: 'What type of employee would you like to add?',
-// //         choices: ['Manager','Engineer','Intern','My Roster Is Complete'],
-// //         }]).then(function(result) {
-// //             switch(result.addEmployee) {
-// //                 case 'Manager': 
-// //             }
-// //         })
-// // }
+        if(role === 'Engineer'){
+            employee = new Engineer(name, id, email, github);
+        }
 
- 
+        if (role === 'Intern'){
+            employee = new Intern(name, id, email, school)
+        }
+
+        console.log(employee);
+        roster.push(employee);
+
+        if (addEmployee){
+            return addTeamMember(roster);
+        } else{
+            return roster;
+        }
+        
+    })
+   
+};
+
+addManager();
